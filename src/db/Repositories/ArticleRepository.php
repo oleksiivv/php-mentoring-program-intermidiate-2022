a16645fe84b7032be5c659d81ca86dcd63d93258
unknown <oleksiypetrochenko@gmail.com>
2022-07-11 10:42:08 +0300

    HT-4 remove deprecated code

--- src/db/Repositories/ArticleRepository.php
+++ src/db/Repositories/ArticleRepository.php
@@ -18,39 +18,6 @@ class ArticleRepository
         return $this->entityManager->getRepository(Article::class)->findBy($criteria, $order, $offset, $start);
     }
 
-    /**
-     * @deprecated
-     */
-    public function indexWithQueryBuilder(int $start=0, int $offset=10, ?array $orderBy = null, ?array $criteria = null): array
-    {
-        $queryBuilder = $this->entityManager->createQueryBuilder();
-
-        $queryBuilder->select('article')
-            ->from(Article::class, 'article');
-
-        if (isset($orderBy)) {
-            $queryBuilder->orderBy('article.' . $orderBy['field'], $orderBy['order']);
-        }
-
-        $result = [];
-
-        foreach ($this->paginate($queryBuilder, $start, $offset) as $entity) {
-            $result[] = $entity;
-        }
-
-        return $result;
-    }
-
-    public function paginate(QueryBuilder $queryBuilder, int $start, int $offset): Paginator
-    {
-        $queryBuilder->setFirstResult($start)
-            ->setMaxResults($start + $offset);
-
-        $paginator = new Paginator($queryBuilder->getQuery(), true);
-
-        return $paginator;
-    }
-
     public function find(int $id): ?Article
     {
         return $this->entityManager->find(Article::class, $id);
